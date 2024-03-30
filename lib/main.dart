78a73f52abcd776d1a7169a52bdd8fe4c0a61ce5
AbhishekASLK <bhoslea925@gmail.com>
2024-03-30 16:06:25 +0530

    file-structured

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ List<ToDoModelClass> tasksFromDB = [];
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   database = openDatabase(
-    join(await getDatabasesPath(), 'ToDoDB3.db'),
+    join(await getDatabasesPath(), 'ToDoDB6.db'),
     version: 1,
     onCreate: (db, version) {
       db.execute(
